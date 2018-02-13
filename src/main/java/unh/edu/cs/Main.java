60fdbbca987de7aa6315d8516fba1fc777241d14
Vadavim <vadavim@gmail.com>
2018-02-13 16:56:52 -0500

    Initial commit.

--- src/main/java/unh/edu/cs/Main.java
+++ src/main/java/unh/edu/cs/Main.java
@@ -1,4 +1,10 @@
 package unh.edu.cs;
 
+
+// This class is not needed, but I am testing to make sure it's inter-operable with Kotlin
 public class Main {
+
+    public static void main(String[] args) {
+        unh.edu.cs.MainKt.main(args);
+    }
 }
--- src/main/kotlin/unh.edu.cs/Main.kt
+++ src/main/kotlin/unh.edu.cs/Main.kt
@@ -1,6 +1,11 @@
 package unh.edu.cs
 
+import org.nd4j.linalg.factory.Nd4j;
+import java.io.File
 
 fun main(args: Array<String>) {
-    println("HI");
+    val graphLocation = "graph.txt"
+//    val out = File(graphLocation).bufferedReader()
+//    out.forEachLine { println(it) }
+    println("HI")
 }

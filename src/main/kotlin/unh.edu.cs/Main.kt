ac451b28e20f1e6afe49ea01daa92f12028afade
Vadavim <vadavim@gmail.com>
2018-02-14 14:32:32 -0500

    Initial commit.

--- src/main/kotlin/unh.edu.cs/Main.kt
+++ src/main/kotlin/unh.edu.cs/Main.kt
@@ -1,11 +1,65 @@
 package unh.edu.cs
 
-import org.nd4j.linalg.factory.Nd4j;
 import java.io.File
 
+val incomingLinks: HashMap<String, ArrayList<String>> = HashMap()           // Nodes that link to node (key)
+val scores: HashMap<String, Double> = HashMap()                             // Current scores of the nodes
+val nodeList: ArrayList<String> = ArrayList()                               // List of all nodes
+const val tChance: Double = 0.85
+val outDegree: HashMap<String, Int> = HashMap()
+val argOptions: HashMap<String, String> = HashMap()
+
+// Reads node adjacency list
+fun parsePages(filename: String) {
+    File(filename)
+            .bufferedReader()
+            .forEachLine {
+                val elements = it.split("\t")
+                val originNode = elements[0]
+                val outGoingNodes = elements.drop(1).toList()
+
+                nodeList += originNode
+                scores[originNode] = 0.0
+                outDegree[originNode] = outGoingNodes.size
+                outGoingNodes.forEach { incomingLinks.getOrPut(it, {ArrayList()}) += originNode}
+            }
+}
+
+fun doBackup(node: String) {
+    val total = incomingLinks[node]?.sumByDouble { scores[it]!! / outDegree[it]!! } ?: 0.0
+    scores[node] = (1 - tChance) / nodeList.size + tChance * total
+}
+
+fun doPageRank() {
+    (0..50000).forEach {
+        nodeList.forEach { doBackup(it) }
+    }
+}
+
+fun printScore() {
+
+    // renormalize values
+    scores.entries.sumByDouble { it.value }
+            .let { total -> scores.replaceAll { key, value -> value / total } }
+
+    // print top 10
+    scores.entries.sortedByDescending { it.value }
+            .take(10)
+            .forEach { (k,v) -> println("$k: $v")  }
+}
+
+fun parseCommands(args: Array<String>) {
+    var cur = ""
+    args.forEach { if (it.startsWith("-")) cur = it else argOptions[cur] = it }
+}
+
 fun main(args: Array<String>) {
-    val graphLocation = "graph.txt"
+    parseCommands(args)
+    argOptions.forEach { t, u -> println("$t: $u")  }
+//    val graphLocation = "graph.txt"
+//    parsePages(graphLocation)
+//    doPageRank()
+//    printScore()
 //    val out = File(graphLocation).bufferedReader()
 //    out.forEachLine { println(it) }
-    println("HI")
 }
